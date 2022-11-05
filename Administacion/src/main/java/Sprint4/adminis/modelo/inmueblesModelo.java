197ae35cf80f4b4fa5c7895e5d6a373e1909728f
jcsalamancse <juan.salamanca98@gmail.com>
2022-11-05 16:45:27 -0500

    modelo

--- Administacion/src/main/java/Sprint4/adminis/modelo/inmueblesModelo.java
+++ Administacion/src/main/java/Sprint4/adminis/modelo/inmueblesModelo.java
@@ -8,25 +8,42 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import javax.persistence.Column;
+import javax.validation.constraints.Size;
 
 @Entity
+
 @Table(name = "tbl_inmuebles")
 
 public class inmueblesModelo implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_Inmuebles;
 
+    private Long id_Inmuebles;
+    @NotEmpty(message = "no puede estar vacio")
+    @Size(min = 4, max = 12, message = "el tamaño tiene que estar entre 4 y 12")
+    @Column(nullable = false, length = 30)
     private Long nombre_inmueble;
     @NotEmpty(message = "no puede estar vacio")
     @Column(nullable = false, length = 30)
+
+    public Long getId_Inmuebles() {
+        return id_Inmuebles;
+    }
+
+    public void setId_Inmuebles(Long id_Inmuebles) {
+        this.id_Inmuebles = id_Inmuebles;
+    }
+
+    public Long getNombre_inmueble() {
+        return nombre_inmueble;
+    }
+
+    public void setNombre_inmueble(Long nombre_inmueble) {
+        this.nombre_inmueble = nombre_inmueble;
+    }
   
-
-
+    
+    
 }
